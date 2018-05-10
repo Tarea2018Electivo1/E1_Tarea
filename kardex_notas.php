65b5b00a4b02cdc693cbb23e91f540a390b05cb3
Brander <Brander@192.168.1.35>
2018-05-10 13:08:28 -0500

    kardex

---
+++ kardex_notas.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Boleta Notas</title>
+</head>
+<body>
+	<fieldset>
+		<legend >Boleta de Notas</legend>
+		<table border="5">
+			<tr>
+				<td>Nota de Trabajos</td>
+				<td>11</td>
+			</tr>
+			<tr>
+				<td>Nota de participacion</td>
+				<td>11</td>				
+			</tr>
+			<tr>
+				<td>Examen Parcial</td>
+				<td>11</td>	
+			</tr>
+			<tr class="promedio">
+				<td>Nota de Unidad</td>
+				<td>11.4</td>		
+			</tr>
+		</table>
+	</fieldset>
+</body>
+</html>
+<style>
+	.promedio{
+		font-weight: bold;
+		background: blue;
+	}
+
+	legend{
+		font-weight: bold;
+	}
+</style>
