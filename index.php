aea9b34cb9f4154e132bdc897ff962451a94cbf5
Brander <Brander@192.168.1.35>
2018-05-10 13:08:20 -0500

    index

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Electivo 1</title>
+</head>
+<body>
+	<div class="contenido">
+		<h2>Electivo 1 - Semestre I - 2018</h2>
+		
+		<fieldset>
+			<legend>Alumnos</legend>
+			<table>
+				<tr>
+					<th>Cod.</th>
+					<th>Nombres y apellidos</th>
+					<th>Opcion</th>
+				</tr>
+				<tr>
+					<td>001</td>
+					<td>Josue Rodriguez Alva</td>
+					<td><a href="registro_notas.html">Ingresar notas de unidad</a></td>
+				</tr>
+				<tr>
+					<td>002</td>
+					<td>Jostein Huerta Flores</td>
+					<td><a href="registro_notas.html">Ingresar notas de unidad</a></td>
+				</tr>
+				<tr>
+					<td>003</td>
+					<td>Alejandro Iturrizaga</td>
+					<td><a href="registro_notas.html">Ingresar notas de unidad</a></td>
+				</tr>
+				<tr>
+					<td>004</td>
+					<td>Pablo Alva Alva</td>
+					<td><a href="registro_notas.html">Ingresar notas de unidad</a></td>
+				</tr>
+				<tr>
+					<td>005</td>
+					<td>Kevin Silva Lopez</td>
+					<td><a href="registro_notas.html">Ingresar notas de unidad</a></td>
+				</tr>
+			</table>
+		
+		</fieldset>
+	</div>
+</body>
+</html>
+<style>
+	.promedio{
+		font-weight: bold;
+		background: red;
+	}
+
+	legend{
+		font-weight: bold;
+	}
+</style>
