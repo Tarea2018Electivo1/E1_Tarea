1bc7837f239e9c3740218b39ec7713bc458aaf55
Brander <Brander@192.168.1.35>
2018-05-10 13:08:39 -0500

    notas

---
+++ registro_notas.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Registro de Notas</title>
+</head>
+<body>
+	<fieldset>
+		<legend>Registro de notas.</legend>
+		<label for="">Unidad :</label>
+		<select name="">
+			<option value="">1</option>
+			<option value="">2</option>
+		</select>
+		<hr>
+		<fieldset>
+			<table>
+				<tr>
+					<td>Nota de Trabajos</td>
+					<td>Nota de participacion</td>
+					<td>Examen Parcial</td>
+					<td>Promedio de Unidad</td>
+				</tr>
+				<tr>
+					<td><input type="text"></td>
+					<td><input type="text"></td>
+					<td><input type="text"></td>
+					<td><input type="text" readonly=""></td>
+				</tr>
+
+			</table>
+			<button class="">Registrar</button>
+		</fieldset>
+	</fieldset>
+</body>
+</html>
